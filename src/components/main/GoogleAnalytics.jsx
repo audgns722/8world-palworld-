9389262b93449af6a46912ddc70a94428baebcb5
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-02-02 19:45:20 +0900

    20240202

---
+++ src/components/main/GoogleAnalytics.jsx
@@ -0,0 +1,16 @@
+export default function GoogleAnalytics() {
+  return (
+    <>
+      {/* Google Tag Manager (noscript) */}
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-KH6RHHQS"
+          height="0"
+          width="0"
+          style="display:none;visibility:hidden"
+        ></iframe>
+      </noscript>
+      {/* End Google Tag Manager (noscript) */}
+    </>
+  );
+}
