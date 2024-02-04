915d7291c2a98eb1fa2e74343256dec00968d418
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-02-04 13:52:10 +0900

    20240204

--- src/app/breeding/page.js
+++ src/app/breeding/page.js
@@ -2,7 +2,7 @@ import Breeding from "@/components/breeding/Breeding";
 
 export const metadata = {
   title: '팰월드 교배 계산기 - 8World',
-  description: '팰 교배 결과를 미리 확인할 수 있습니다.',
+  description: '팰 교배 계산기(교배표)로 결과를 미리 확인할 수 있습니다.',
 };
 
 export default function page() {
