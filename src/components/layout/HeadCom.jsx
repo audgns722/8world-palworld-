be4c556afec91f5a088737c2e8b34031ede3238e
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-02-08 19:46:10 +0900

    20240208

--- src/components/layout/HeadCom.jsx
+++ src/components/layout/HeadCom.jsx
@@ -6,8 +6,8 @@ import Head from 'next/head'
 export default function HeadCom() {
     return (
         <Head>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4313698984582740"
-                crossorigin="anonymous"></script>
+            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4313698984582740" crossorigin="anonymous"></script>
+            <meta name="google-adsense-account" content="ca-pub-4313698984582740"></meta>
         </Head>
     )
 }
