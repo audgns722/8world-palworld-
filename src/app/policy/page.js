2950c1b5b3a78e560c93304fb551aba8ba982fc0
yunyoungsik <yunyoungsik91@gmail.com>
2024-02-08 14:13:36 +0900

    20240208

---
+++ src/app/policy/page.js
@@ -0,0 +1,124 @@
+export const metadata = {
+  title: '개인정보처리방침 - 8World',
+  description: '팰월드 맵 개인정보처리방침을 확인할 수 있습니다.',
+};
+
+export default function Page() {
+
+  return (
+    <main id="main" role="main">
+      <div className="home__inner">
+        <div className="home__title">
+          <h2>8Worl:D</h2>
+        </div>
+        <div className="policy container">
+          <h2>개인정보처리방침</h2>
+          <div className="box">
+            <h3>1. 개인정보 수집 목적</h3>
+            <div className="text">
+              <p>우리는 다음과 같은 목적으로 개인정보를 수집합니다:</p>
+              <ul>
+                <li>- 광고 서비스 제공을 위한 정보 수집</li>
+                <li>- 이용자와의 원활한 소통을 위한 정보 수집</li>
+                <li>- 법적 의무의 이행을 위한 정보 수집</li>
+                <li>- 기타 서비스 제공을 위한 필요한 정보 수집</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>2. 수집하는 개인정보 항목</h3>
+            <div className="text">
+              <p>우리는 다음과 같은 개인정보를 수집할 수 있습니다:</p>
+              <ul>
+                <li>- 성명</li>
+                <li>- 연락처 정보 (이메일 주소, 전화번호 등)</li>
+                <li>- 광고 선호도 및 관심사에 대한 정보</li>
+                <li>- 디바이스 정보 (IP 주소, 쿠키 등)</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>3. 개인정보 보유 및 이용 기간</h3>
+            <div className="text">
+              <p>우리는 개인정보를 수집한 목적을 달성한 후에는 지체 없이 파기합니다. 그러나 관련 법령에 따라 보존이 필요한 경우에는 해당 기간 동안 보관할 수 있습니다.</p>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>4. 개인정보의 제3자 제공</h3>
+            <div className="text">
+              <p>우리는 이용자의 동의 없이 개인정보를 외부에 제공하지 않습니다. 단, 다음과 같은 경우에는 제3자에게 제공될 수 있습니다:</p>
+              <ul>
+                <li>- 법령에 따라 제공이 요구되는 경우</li>
+                <li>- 서비스 제공을 위해 협력하는 파트너에게 제공되는 경우</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>5. 이용자의 권리</h3>
+            <div className="text">
+              <p>이용자는 개인정보에 대한 열람, 정정, 삭제 등의 권리를 가지고 있습니다. 이를 위해서는 문의처로 연락하시기 바랍니다.</p>
+              <ul>
+                <li>- 법령에 따라 제공이 요구되는 경우</li>
+                <li>- 서비스 제공을 위해 협력하는 파트너에게 제공되는 경우</li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>6. 쿠키 및 유사 기술의 사용</h3>
+            <div className="text">
+              <p>우리는 쿠키 및 유사한 기술을 사용하여 이용자의 경험을 개선하고 서비스를 제공합니다. 이에 대한 자세한 내용은 쿠키 정책을 참고하시기 바랍니다.</p>
+              <ul>
+              <li>- 법령에 따라 제공이 요구되는 경우</li>
+              <li>- 서비스 제공을 위해 협력하는 파트너에게 제공되는 경우</li>
+            </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>6. 쿠키 및 유사 기술의 사용</h3>
+            <div className="text">
+              <p>우리는 쿠키 및 유사한 기술을 사용하여 이용자의 경험을 개선하고 서비스를 제공합니다. 이에 대한 자세한 내용은 쿠키 정책을 참고하시기 바랍니다.</p>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>7. 개인정보 보안 조치</h3>
+            <div className="text">
+              <p>우리는 적절한 기술적, 물리적, 관리적 조치를 통해 개인정보를 안전하게 보호합니다.</p>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>8. 개인정보 관리책임자</h3>
+            <div className="text">
+              <p>개인정보 보호 관련 문의 및 민원 처리를 위해 아래의 연락처로 문의하시기 바랍니다:</p>
+              <ul>
+              <li>- M6Y4</li>
+              <li>- pal_world@outlook.kr</li>
+            </ul>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>9. 개인정보 처리방침 변경</h3>
+            <div className="text">
+              <p>우리는 필요한 경우 개인정보 처리방침을 변경할 수 있습니다. 변경 사항은 웹사이트를 통해 공지됩니다.</p>
+            </div>
+          </div>
+
+          <div className="box">
+            <h3>10. 유의사항</h3>
+            <div className="text">
+              <p>이 개인정보 처리방침은 Google AdSense의 유럽 규정에 맞추어 작성되었으며, 규정이 변경될 경우 업데이트될 수 있습니다. 최신 정보를 확인하시기 바랍니다.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
