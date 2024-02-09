0411c68510c8f258c54de5ecba31bb5dd506b047
yunyoungsik <144635640+yunyoungsik@users.noreply.github.com>
2024-02-09 19:58:09 +0900

    20240209

--- src/components/layout/HeadCom.jsx
+++ src/components/layout/HeadCom.jsx
@@ -1,13 +1,29 @@
-"use client"
+'use client';
 
-import React from 'react'
-import Head from 'next/head'
+import React from 'react';
+import Head from 'next/head';
+import Script from 'next/script';
 
 export default function HeadCom() {
-    return (
-        <Head>
-            <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4313698984582740" crossorigin="anonymous"></script>
-            <meta name="google-adsense-account" content="ca-pub-4313698984582740"></meta>
-        </Head>
-    )
+  return (
+    <Head>
+      <>
+        <Script
+          async
+          src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4313698984582740`}
+          strategy="lazyOnload"
+          crossOrigin="anonymous"
+        />
+        {/* <script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4313698984582740"
+        crossorigin="anonymous"
+      ></script> */}
+        <meta
+          name="google-adsense-account"
+          content="ca-pub-4313698984582740"
+        ></meta>
+      </>
+    </Head>
+  );
 }
